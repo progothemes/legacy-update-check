efa6d3c074d7fdae2928660f70dfee499c9f7c9a
alex chousmith <alex@ninthlink.com>
2011-02-17 19:19:30 +0000

    cleaned up keygen process

--- keygen.php
+++ keygen.php
@@ -2,18 +2,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>ProGo Key Gen?</title>
 </head>
 
 <body>
 <?php
-// d1196c1c8ab714ec163335d1a988ae3f
-
 	//$url = 'http://www.ninthlink.com/direct/';
 	$email = $_GET['email'];
 	$currtime = date('Y-m-d H:i:s');
 	$api_key = md5(crypt("$email : $currtime"));
-	echo "<h1>API Key $api_key</h1>";
+	echo "<h1>Here is a new API Key</h1><p>for<br /><strong>email address</strong> $email<br /><strong>created at</strong> $currtime</p>";
+	echo "<h3>API KEY for the DB</h3><input type='text' readonly='readonly' value='$api_key' size='50' onfocus='this.select();' />";
+	$nice_key = implode( '-', str_split( strtoupper( $api_key ), 4) );
+	echo "<h3>Human-Friendly API Key</h3><input type='text' readonly='readonly' value='$nice_key' size='50' onfocus='this.select();' />";
 ?>
 </body>
 </html>
