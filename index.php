9f3e7f1af67bf4774d6f428cc0b7337abdfb4008
alex chousmith <alex@ninthlink.com>
2011-03-01 00:16:50 +0000

    added code to specify max dimensions of Custom Logo and direct response page top left image overrides. also adjusted the Page Preview area when editing Direct Response pages, for the new colorschemes. and fixed the SALE price color display for new colorschemes.

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 // Theme with update info
 $packages['direct'] = array(
 	'versions' => array(
-		'1.0.52' => array(
-			'version' => '1.0.52',
-			'date' => '2011-02-24',
+		'1.0.53' => array(
+			'version' => '1.0.53',
+			'date' => '2011-02-28',
 			'package' => 'http://www.progothemes.com/direct.zip'
 		)
 	),
