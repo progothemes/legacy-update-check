849e53224b7bafe7a571491484416f0c097df387
alex chousmith <alex@ninthlink.com>
2011-09-28 18:47:07 +0000

    [BugzID:3774] 1.2.1 : fixed THEME PREVIEW ability in all 3 main themes, and also added some more-helpful text per fb #3774

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
 // Theme with update info
 $packages = array(
 	'direct' => array(
-		'latest' => '1.2.0',
-		'date' => '2011-09-26',
+		'latest' => '1.2.1',
+		'date' => '2011-09-27',
 		'info' => 'http://www.progo.com'
 	),
 	'ecommerce' => array(
-		'latest' => '1.2.0',
-		'date' => '2011-09-26',
+		'latest' => '1.2.1',
+		'date' => '2011-09-27',
 		'info' => 'http://www.progo.com'
 	),
 	'businesspro' => array(
-		'latest' => '1.2.0',
-		'date' => '2011-09-26',
+		'latest' => '1.2.1',
+		'date' => '2011-09-27',
 		'info' => 'http://www.progo.com'
 	),
 	'bookit' => array(
